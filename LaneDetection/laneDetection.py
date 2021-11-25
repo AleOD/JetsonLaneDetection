20ebd989fcef4effbaeda825c6c290efdf5438e9
AsielTrejo <asieltrejo@gmail.com>
2021-11-25 15:10:14 -0600

    try

--- LaneDetection/laneDetection.py
+++ LaneDetection/laneDetection.py
@@ -39,7 +39,7 @@ def canny(img):
     blur = cv2.GaussianBlur(gray,(kernel, kernel),0)
     canny = blur
     #canny = cv2.Canny(gray, 4, 100,L2gradient = True)
-    canny = cv2.Canny(gray, 350, 400)
+    canny = cv2.Canny(gray, 1000, 900)
     return canny
 
 def region_of_interest(canny):
