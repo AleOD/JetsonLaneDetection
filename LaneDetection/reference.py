e36d07be7b92e553dcaedec48994cb709e088a2f
AsielTrejo <asieltrejo@gmail.com>
2021-11-25 13:54:22 -0600

    vote 15

--- LaneDetection/reference.py
+++ LaneDetection/reference.py
@@ -54,8 +54,8 @@ def region_of_interest(canny):
    return masked_image
 
 def houghLines(cropped_canny):
-   return cv2.HoughLinesP(cropped_canny, 2, np.pi/180, 100, 
-       np.array([]), minLineLength=40, maxLineGap=5)
+   return cv2.HoughLinesP(cropped_canny, 10, np.pi/180, 20, 
+       np.array([]), minLineLength=40, maxLineGap=200)
 
 def addWeighted(frame, line_image):
     return cv2.addWeighted(frame, 0.8, line_image, 1, 1)
