2d3ab10b2bc2418f9cb369e8cd0b248b9a3dee49
GUPABE10 <GUPABE10@gmail.com>
2021-11-26 13:44:22 -0600

    hough

--- LaneDetection/laneDetection.py
+++ LaneDetection/laneDetection.py
@@ -124,10 +124,10 @@ def average_slope_intercept(image, lines):
         right_line = make_points(image, right_fit_average)
         print(right_line)
     
-    left_fit_average  = np.average(left_fit, axis=0) 
-    left_line  = make_points(image, left_fit_average)
-    right_fit_average = np.average(right_fit, axis=0)
-    right_line = make_points(image, right_fit_average)
+    #left_fit_average  = np.average(left_fit, axis=0) 
+    #left_line  = make_points(image, left_fit_average)
+    #right_fit_average = np.average(right_fit, axis=0)
+    #right_line = make_points(image, right_fit_average)
 
     averaged_lines = [left_line, right_line]
     return averaged_lines
