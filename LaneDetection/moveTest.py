3f0939da493b8944d23a374907d5e49899b2984d
AsielTrejo <asieltrejo@gmail.com>
2021-12-01 14:04:34 -0600

    just erosion

--- LaneDetection/moveTest.py
+++ LaneDetection/moveTest.py
@@ -58,13 +58,13 @@ def region_of_interest(canny):
    cv2.fillPoly(mask, trapezoid, 255)
    #cv2.fillPoly(mask, trapezoid, 0)
    masked_image = cv2.bitwise_and(canny, mask)
-   kernel = np.ones((7,7),np.uint8)
-   erosion = cv2.erode(masked_image,kernel,iterations = 3)
-   dilation = cv2.dilate(masked_image,kernel,iterations = 3)
-   erosion2 = cv2.erode(masked_image,kernel,iterations = 1)
+   kernel = np.ones((9,9),np.uint8)
+   erosion = cv2.erode(masked_image,kernel,iterations = 2)
+   dilation = cv2.dilate(erosion,kernel,iterations = 2)
+   #erosion2 = cv2.erode(masked_image,kernel,iterations = 2)
    #cv2.polylines(masked_image, trapezoid, True, 255, 2)
    #return dilation
-   return erosion2
+   return dilation
 
 def houghLines(cropped_canny):
    return cv2.HoughLinesP(cropped_canny, 3, np.pi/180, 3, 
