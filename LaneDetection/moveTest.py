c377468951254d7fc62ad2a5c66ab23413f6da0f
AsielTrejo <asieltrejo@gmail.com>
2021-12-01 14:41:11 -0600

    added erosion and dilation

--- LaneDetection/moveTest.py
+++ LaneDetection/moveTest.py
@@ -40,7 +40,12 @@ def canny(img):
         cap.release()
         cv2.destroyAllWindows()
         exit()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    kernel = np.ones((3,3),np.uint8)
+    erosion = cv2.erode(img,kernel,iterations = 1)
+    dilation = cv2.dilate(erosion,kernel,iterations = 4)
+        
+    gray = cv2.cvtColor(dilation, cv2.COLOR_BGR2GRAY)
     kernel = 101
     blur = cv2.GaussianBlur(gray,(kernel, kernel),0)
     canny = blur
