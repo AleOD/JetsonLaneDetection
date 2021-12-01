f978c9422794c3bcf372d4b391602fc3e0e7bb2e
AsielTrejo <asieltrejo@gmail.com>
2021-12-01 13:59:02 -0600

    just erosion

--- LaneDetection/moveTest.py
+++ LaneDetection/moveTest.py
@@ -60,7 +60,7 @@ def region_of_interest(canny):
    masked_image = cv2.bitwise_and(canny, mask)
    kernel = np.ones((7,7),np.uint8)
    erosion = cv2.erode(masked_image,kernel,iterations = 3)
-   dilation = cv2.dilate(masked_image,kernel,iterations = 3)
+   dilation = cv2.dilate(masked_image,kernel,iterations = 2)
    #cv2.polylines(masked_image, trapezoid, True, 255, 2)
    return dilation
    #return erosion
