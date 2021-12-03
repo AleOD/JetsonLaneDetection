fa7283dcb3573a525ff6c16c4ccacfff6d009f7f
GUPABE10 <GUPABE10@gmail.com>
2021-12-03 17:50:26 -0600

    new file testOffset

---
+++ LaneDetection/testOffset.py
@@ -0,0 +1,14 @@
+import rospy
+from std_msgs.msg import Float32
+
+while True:
+    pub_throttle = rospy.Publisher('throttle', Float32, queue_size=8)
+    pub_steering = rospy.Publisher('steering', Float32, queue_size=8)
+    rospy.init_node('teleop', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+
+    val = input()
+    pub_throttle.publish(-0.2)
+    pub_steering.publish(val)
+
+    rate.sleep()
