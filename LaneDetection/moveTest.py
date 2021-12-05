b398e6a017c229019a43847b09f422ad057311ce
AsielTrejo <asieltrejo@gmail.com>
2021-12-04 18:11:40 -0600

    houghed

--- LaneDetection/moveTest.py
+++ LaneDetection/moveTest.py
@@ -127,8 +127,8 @@ def region_of_interest(canny):
    return masked_image
 
 def houghLines(cropped_canny,width):
-   return cv2.HoughLinesP(cropped_canny, 4, np.pi/180, 1, 
-       None, minLineLength=0, maxLineGap=0)
+   return cv2.HoughLinesP(cropped_canny, 1, np.pi/180, 1, 
+       np.array([]), minLineLength=0, maxLineGap=0)
 def addWeighted(frame, line_image):
     return cv2.addWeighted(frame, 0.8, line_image, 1, 0.0)
  
