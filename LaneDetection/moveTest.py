ed0ceac3846317077b9c3bda8f53353e9986d915
AsielTrejo <asieltrejo@gmail.com>
2021-12-05 20:57:50 -0600

    Reset to throttle 0.4 chido

--- LaneDetection/moveTest.py
+++ LaneDetection/moveTest.py
@@ -241,7 +241,7 @@ def average_slope_intercept(image, lines):
 def movement(slopeVal,pub_throttle,pub_steering):
     slopeLeft=slopeVal[0]
     slopeRight=slopeVal[1]
-    caso = 0
+    caso = 0 #
     #pub_throttle.publish(-0.3)
     print("********pendiente izquierda")
     print(slopeLeft)
@@ -251,40 +251,40 @@ def movement(slopeVal,pub_throttle,pub_steering):
 
     #steeringVal = 0.0
     if((-slopeLeft > setpoint) and (slopeRight > setpoint)): #Caso 1
-        throttleVal = -0.5
+        throttleVal = -0.4
         steeringVal = 0.0
         caso = 1
     elif(-slopeLeft > 0.0 and -slopeLeft < setpoint): #Caso 4,6,8
         if (slopeRight > 0.0 and slopeRight < setpoint): #Caso 8
-            throttleVal = -0.7
+            throttleVal = -0.4
             steeringVal = 0.0
             caso = 8
         elif (slopeRight == 0.0): #Caso 6
             throttleVal = -lapSpeed
             #steeringVal = _map(-slopeLeft, 0.1, setpoint, 0.95, 0.1)
-            steeringVal = 0.85 + offsetRight
+            steeringVal = 0.45 + offsetRight
             caso = 6
         else: #Caso 4
-            throttleVal = -0.5
+            throttleVal = -0.4
             steeringVal = 0.0 ## avanzar
             caso = 4
     elif(slopeRight > 0.0 and slopeRight < setpoint): #Caso 2,7
         if(slopeLeft==0.0): #Caso 7
             throttleVal = -lapSpeed
             #steeringVal = _map(slopeRight, 0.1, setpoint, 0.95, 0.1)
-            steeringVal = -0.85
+            steeringVal = -0.45
             caso = 7
         else: #Caso 2
-            throttleVal = -0.5
+            throttleVal = -0.4
             steeringVal = -0.0
             caso = 2
     elif(-slopeLeft > setpoint and slopeRight==0.0): #Caso 3
         throttleVal = -0.4
-        steeringVal = 0.95 + offsetRight
+        steeringVal = 0.55 + offsetRight
         caso = 3
     elif(slopeRight > setpoint and slopeLeft==0.0): #Caso 5
         throttleVal = -0.4
-        steeringVal = -0.75
+        steeringVal = -0.55
         caso = 5
     #steeringVal = computePID(slopeLeft+slopeRight)
     pub_steering.publish(steeringVal)
@@ -307,7 +307,7 @@ def mainCamera():
     pub_throttle = rospy.Publisher('throttle', Float32, queue_size=8)
     pub_steering = rospy.Publisher('steering', Float32, queue_size=8)
     rospy.init_node('teleop', anonymous=True)
-    rate = rospy.Rate(1000) # 100hz
+    rate = rospy.Rate(200) # 100hz
     caso = 0
 
     #pub_throttle.publish(-1.0)
